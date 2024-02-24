9765efc62a6e80aef1753231af69a2feb321bbf1
79612 <79612@DESKTOP-O1P5TC5>
2024-02-24 10:31:27 +0300

    Добавьте файлы проекта.

---
+++ exit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string exitWord = "exit";
+            bool cycleWork = true;
+
+            while(cycleWork == true)
+            {
+                Console.WriteLine("для выхода введите exit");
+                string userInput = Console.ReadLine();
+
+                if (exitWord == userInput) 
+                {
+                   cycleWork = false;
+                }
+            }
+
+        }
+
+    }
+}
